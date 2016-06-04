52273da507048bd4e994dfb2e2747307ec9dec05
Vinicius Ortiz <visortiz@gmail.com>
2016-06-04 16:43:41 -0300

    Finalizacao do app

--- app/scripts/controllers/list.ctrl.js
+++ app/scripts/controllers/list.ctrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * # MainCtrl
+ * # ListCtrl
  */
 angular.module(APP_NAME)
   .controller('ListCtrl', ListCtrl);
@@ -12,9 +12,11 @@ function ListCtrl($scope, list) {
   this.parallax = this.parallax || $('.parallax');
   this.modalTrigger = this.modalTrigger || $('.modal-trigger');
   this.list = list.data;
+  this.formModel = {};
 
   this.initParallax = _initParallax();
-  this.saveNewNote = _saveNewNote;
+  this.saveNote = _saveNote;
+  this.editNote = _editNote;
   this.deleteNote = _deleteNote;
 
   //  ///////////////////////////////////
@@ -23,32 +25,64 @@ function ListCtrl($scope, list) {
     vm.parallax.parallax();
   }
 
-  function _cleanForm() {
-    vm.title = '';
-    vm.text = '';
+  function _saveNote() {
+    if (!vm.formModel.title || !vm.formModel.text) return;
+
+    if (vm.formModel.id) { _updateNote(); }
+    else { _createNote(); }
+    _cleanForm();
+
+    ////////
+
+    function _updateNote() {
+      var note = _findNoteById(vm.formModel.id);
+      note.title = vm.formModel.title;
+      note.text = vm.formModel.text;
+    }
+
+    function _createNote() {
+      vm.list.push( {
+        id: vm.list.length + 1,
+        title: vm.formModel.title,
+        text: vm.formModel.text
+      });
+    }
   }
 
-  function _saveNewNote() {
-    if (!vm.title || !vm.text) return;
-
-    vm.list.push(
-      {
-        id: vm.list.length + 1,
-        title: vm.title,
-        text: vm.text
-      }
-    );
-    _cleanForm();
+  function _editNote(id) {
+    var note = _findNoteById(id);
+    vm.formModel.id = note.id;
+    vm.formModel.title = note.title;
+    vm.formModel.text = note.text;
+    _goTop();
   }
 
   function _deleteNote(id) {
-    vm.list.some(function(element, idx) {
+    var note = _findNoteById(id)
+    vm.list.splice(vm.list.indexOf(note), 1);
+    $scope.$apply();
+    $('#modal_' + id).closeModal();
+    $('.lean-overlay').remove();
+  }
+
+  function _findNoteById(id) {
+    var note;
+    vm.list.some(function (element) {
       if (element.id === id) {
-        vm.list.splice(vm.list.indexOf(element), 1);
-        $scope.$apply();
-        $('#modal_' + id).closeModal();
-        $('.lean-overlay').remove();
+        note = element;
+        return true;
       }
     });
+    return note;
+  }
+
+  function _cleanForm() {
+    vm.formModel = {};
+  }
+
+  function _goTop() {
+    var body = body || $('body');
+    var element = angular.element(vm.parallax);
+    angular.element(body).animate({scrollTop: element.offset().top}, "slow");
   }
 }
